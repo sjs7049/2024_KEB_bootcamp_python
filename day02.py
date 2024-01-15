b272011c4fe865e68a5ca9dd87a476089fac5028
sjs7049 <sjs7049@gmail.com>
2024-01-15 16:15:49 +0900

    v1.4 python day02 homework

--- day02.py
+++ day02.py
@@ -1,11 +1,17 @@
-letter = input("Input alphabet letter : ")
-vowels = {'a','e','i','o','u'}
-print(type(vowels))
+# 1과 2외의 숫자 입력할 때까지 반복문 돌아가도록 만들기 1과 2외의 숫자 입력하면 프로그램 종료하도록
+# 1 입력시: 화씨를 섭씨로 변경한 값 출력
+# 2 입력시: 섭씨를 화씨로 변경한 값 출력
+# 그 외 입력시: 프로그램 종료
 
-if letter in vowels:
-    print(f'{letter} is a vowel.')
-else:
-    print(f'{letter} is a consonant.')
+while True:
+    menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit    3) Quit program : ")
 
-vowels = 'aeiou'
-print(type(vowels))
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}°F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}°C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}°C, Fahrenheit : {((celsius * 9.0 / 5.0) + 32.0):.4f}°F')
+    else:
+        print('Quit program.')
+        break

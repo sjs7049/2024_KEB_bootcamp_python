8d6279eb4f8cb28d904774046b562d05575fc046
sjs7049 <sjs7049@gmail.com>
2024-01-15 14:14:40 +0900

    v0.9 python day02

--- day02.py
+++ day02.py
@@ -1,11 +1,14 @@
-first_number = int(input("First number : "))
-second_number = int(input("Second number : "))
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec,octal,hexadecimal,binary)
+print(chr(dec),chr(octal),chr(hexadecimal),chr(binary))
+print(ord('B'),ord('Z'),ord('a'),ord('2')) # 66 90 97 50
 
-quotient = first_number // second_number
-remainder = first_number % second_number
-print(f'몫은 {quotient} 나머지는 {remainder}입니다.')
+value = 65
+print(bin(value))
+print(oct(value))
+print(hex(value))
 
-first_number = int(input("First number : "))
-second_number = int(input("Second number : "))
 
-print(f'몫은 {divmod(first_number,second_number)[0]} 나머지는 {divmod(first_number,second_number)[1]}입니다.')

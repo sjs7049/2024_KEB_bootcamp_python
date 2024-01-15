9a7474456b7729c65bb914ea233a5b835fcccc1e
sjs7049 <sjs7049@gmail.com>
2024-01-15 13:22:26 +0900

    v0.7 python day02

--- day02.py
+++ day02.py
@@ -9,4 +9,10 @@
 
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input base number : '))
+# f-string
 print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number,exponent_number)}')
+# format function
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number,exponent_number,pow(base_number,exponent_number)))
+# c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number,exponent_number,pow(base_number,exponent_number)))

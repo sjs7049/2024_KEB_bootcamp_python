9cf273084b2d6d033683434c70b180d2fa51e62f
sjs7049 <sjs7049@gmail.com>
2024-01-15 10:56:09 +0900

    v0.4 python day02

--- day02.py
+++ day02.py
@@ -1,19 +1,28 @@
-univ = "Inha university"
-print(univ)
-print(univ[5])
+# univ = "Inha university"
+# print(univ)
+# print(univ[5])
 # univ[5] = 'U' // immmutable
 # print(univ[5])
 
-subjects = ['python','c++','linux','data structure','database']
-print(subjects)
-print(subjects[3])
-subjects[3] = 'data structure & algorithm' # mutable
-print(subjects)
+# subjects = ['python','c++','linux','data structure','database']
+# print(subjects)
+# print(subjects[3])
+# subjects[3] = 'data structure & algorithm' # mutable
+# print(subjects)
 
 
+# case-sensitive
+abc = 7
+Abc = 8
+ABC = 6
+print(abc,Abc,ABC)
 
+test9 = 77
+# 9test = 77 (error: 변수 이름 숫자로 시작 불가)
+_9test = 71
+print(test9,_9test)
 
-
+# False = 123 (error: reserve words)
 
 
 

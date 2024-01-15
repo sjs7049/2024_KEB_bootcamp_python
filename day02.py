56c9e8c1f14bba1eb3c2f1e0d9a954e75ddde171
sjs7049 <sjs7049@gmail.com>
2024-01-15 10:44:51 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,25 @@
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U' // immmutable
+# print(univ[5])
+
+subjects = ['python','c++','linux','data structure','database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm' # mutable
+print(subjects)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

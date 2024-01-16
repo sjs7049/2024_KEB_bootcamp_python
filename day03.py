a298fed073bd8ae417af2885e3ad68250c15893f
sjs7049 <sjs7049@gmail.com>
2024-01-16 13:36:54 +0900

    v2.1 python day03

--- day03.py
+++ day03.py
@@ -1,7 +1,10 @@
-# preview: 예외 던지기
-subjects = "python c++ database linux"
-subject = input("수강신청과목 입력 : ")
-try:
-    print(f'해당 과목은 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.')
-except ValueError:
-    print('해당 과목이 존재하지 않습니다.')
+# # preview: 예외 던지기
+# subjects = "python c++ database linux"
+# print(subjects.isalnum())
+
+# subject = input("수강신청과목 입력 : ")
+# try:
+#     print(f'해당 과목은 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.')
+# except ValueError:
+#     print('해당 과목이 존재하지 않습니다.')
+

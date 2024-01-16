de9c73d507e9c503dacb8e7de6b05ad5669dad33
sjs7049 <sjs7049@gmail.com>
2024-01-16 11:39:45 +0900

    v1.9 python day03

--- day03.py
+++ day03.py
@@ -1,27 +1,6 @@
-course = "2024 KEB Bootcamp"
-print(course.replace('KEB','Inha'))
-print(course)
-
-course = "KEB 2024 KEB Bootcamp"
-print(course.replace('KEB','Inha'))
-print(course)
-
-course = "KEB 2024 KEB Bootcamp KEB...*!#"
-course = course.replace('KEB','Inha',2) # 앞에 두 개의 KEB만 Ihna로 대체됨
-print(course)
-print(course.strip())
-print(course.strip("!#.*"))
-
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-course = course.replace('KEB','Inha',2)
-print(course)
-print(course.strip())
-print(course.strip("!#.*"))
-
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-print(course.find('KEB'))
-print(course.rfind('KEB')) # 역방향으로 탐색
-print(course.index('KEB'))
-print(course.rindex('KEB'))
-print(course.find('Inha')) # result: -1 (찾는 값이 없을 때 -1 출력)
-print(course.index('Inha')) # ValueError: substring not found (예외 던짐)
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
+if subjects.find(subject) != -1:
+    print(f'해당 과목은 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.')
+else:
+    print('해당 과목이 존재하지 않습니다.')

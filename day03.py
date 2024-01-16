9279defed2a28a5956d327d63d494f087192b67d
sjs7049 <sjs7049@gmail.com>
2024-01-16 10:34:54 +0900

    v1.6 python day03

--- day03.py
+++ day03.py
@@ -1,17 +1,16 @@
-# 3 입력할 때까지 반복문 돌아가도록 만들기 3 입력하면 프로그램 종료하도록
-# 1 입력시: 화씨를 섭씨로 변경한 값 출력
-# 2 입력시: 섭씨를 화씨로 변경한 값 출력
-# 그 외 입력시: 프로그램 종료
+university = "Ihha\nUniversity"
+# university = r"Ihha\nUniversity" # raw string
+print(university)
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit    3) Quit program : ")
+number1 = input("First number : ")
+number2 = input("Second number : ")
+print(number1 + number2) # concatenation
+print(number1 * 3) # duplicate
+print(number1 + 3) # type error
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}°F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}°C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}°C, Fahrenheit : {((celsius * 9.0 / 5.0) + 32.0):.4f}°F')
-    elif menu == 3:
-        print('Quit program.')
-        break
+university = "Ihha University!"
+print(university[:4])
+print(university[:-11]) # reverse index
+print(university[:len(university)]) # print(university[:16])
+print(university[::1]) # incresing 1
+print(university[::2]) # incresing 2

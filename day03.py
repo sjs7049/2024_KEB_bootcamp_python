96efe2c2f10886e35f1c8b58563f2c6359d50822
sjs7049 <sjs7049@gmail.com>
2024-01-16 14:37:01 +0900

    v2.5 python day03

--- day03.py
+++ day03.py
@@ -1,13 +1,14 @@
 # prime number
 number = int(input("Input number : "))
-cnt = 0
+is_prime = True
 i = 2
 while i < number:
     if number % i == 0:
-        cnt += 1
+        is_prime = False
+        break
     i += 1
 
-if cnt == 0:
+if is_prime:
     print(f'{number} is prime number.')
 else:
     print(f'{number} is not prime number.')

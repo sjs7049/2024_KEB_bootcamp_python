e56cce27f30c9ecc9f71e40068fc6115c0110df0
sjs7049 <sjs7049@gmail.com>
2024-01-16 14:05:39 +0900

    v2.3 python day03

--- day03.py
+++ day03.py
@@ -1,16 +1,13 @@
-# subjects = {'python' : 'Kim', 'c++' : 'sung', 'data structure' : 'kim', 'database' : 'kang'}
-# print("{0[python]} {0[data structure]}".format(subjects))
-
 # prime number
 number = int(input("Input number : "))
 cnt = 0
-i = 1
-while i <= number:
+i = 2
+while i < number:
     if number % i == 0:
         cnt += 1
     i += 1
 
-if cnt == 2:
+if cnt == 0:
     print(f'{number} is prime number.')
 else:
     print(f'{number} is not prime number.')

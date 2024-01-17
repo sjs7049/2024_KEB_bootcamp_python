9cc92a125493bfe2837933d977551aa4f3358b41
sjs7049 <sjs7049@gmail.com>
2024-01-17 10:35:20 +0900

    v3.4 python day04

--- day04.py
+++ day04.py
@@ -1,23 +1,35 @@
-# prime 2
-numbers = input('Input first & second number : ').split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+t1 = (5) # type: int
+t2 = 5, # type: tuple
+t3 = (5,) # type: tuple
+t4 = (5,7) # type: tuple
+t5 = 5,7 # type: tuple
+print(type(t1),type(t2),type(t3),type(t4),type(t5))
 
-if n1 > n2:
-    n1, n2 = n2, n1
+t6 = "python", "kim" # packing
+print(type(t6), t6[1])
 
-for number in range(n1, n2 + 1):
-    is_prime = True
+subject, prof = t6 # unpacking
+print(prof)
+print(subject)
 
-    if number < 2:
-        pass
-    else:
-        i = 2
-        while i*i <= number:
-            if number % i == 0:
-                is_prime = False
-                break
-            i += 1
-            # print(i, end=' ') # count check
-        if is_prime:
-            print(number, end=' ')
+# packing 개수와 받는 변수 개수 같아야 함
+# a,b,c = t6 # ValueError
+
+t7 = () # type: tuple
+t8 = tuple() # type: tuple
+print(type(t7),type(t8))
+print(type(9,),type((9,))) # type: int, tuple
+
+t9 = 1, 2, 3
+t10 = 1, 2
+print(t9 == t10)
+print(t9 <= t10)
+print(t9 > t10)
+
+t11 = 4.43,
+t12 = 3.97, 4.1, 3.27
+# print(t11 + t12)
+print(id(t11))
+t11 += t12
+print(id(t11))
+print(t11)

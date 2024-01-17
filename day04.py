e108efc0fcf493c2a5fabbb91c392e31759fc03b
sjs7049 <sjs7049@gmail.com>
2024-01-17 13:33:44 +0900

    v3.6 python day04

--- day04.py
+++ day04.py
@@ -1,17 +1,13 @@
-# subjects = ["C++", "Java", "Python"]
-# # print(subjects[::-1])
-# subjects.reverse() # reverse는 할당해서 자체 값을 바꿈
-# print(subjects)
-#
-# subjects.sort()
-# print(subjects)
-#
-# subjects.sort(reverse=True) # 역으로 정렬
-# print(subjects)
+# subjects = ["a", "b", "c"]
+import copy
 
-subjects = ["C++", "5", "Java", "Python", "9"]
-# subjects.sort()
-# print(subjects)
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
+subjects = ["a", ["b", "c"], "d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e)
+

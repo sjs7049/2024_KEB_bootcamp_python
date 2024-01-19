031004092530b6a4f1b53218745f373b35754a96
sjs7049 <sjs7049@gmail.com>
2024-01-19 14:43:13 +0900

    v6.1 python day06

--- day06.py
+++ day06.py
@@ -1,37 +1,31 @@
-# class Pokemon:
-#     def __init__(self, name):
-#         print(f'{name} 포켓몬스터 생성')
-#
-# pikachu = Pokemon('피카츄')
-# squirtle = Pokemon('꼬부기')
-# print(pikachu) # 메모리 주소 출력 <__main__.Pokemon object at 0x000001CDA13D56F0>
-# print(squirtle)
-
-# class Pokemon:
-#     pass
-#
-# pikachu = Pokemon()
-# squirtle = Pokemon()
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# # print(pikachu.nemesis.name)
-# pikachu.nemesis.name = "꼬부기" # squirtle.name = "꼬부기"
-# print(pikachu.nemesis.name)
-# print(squirtle.name)
-
 class Pokemon:
     def __init__(self, name):
-        self.name = name # error 1 해결 - 따라서 이 문구 넣어줘야 함
-        print(f'{name} 포켓몬스터 생성')
-
+        self.name = name
     def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격')
+        print(f'{self.name}이(가) {target.name}을(를) 공격!')
 
-pikachu = Pokemon('피카츄')
-squirtle = Pokemon('꼬부기')
-charizard = Pokemon("리자몽")
-# 해당 시점에 수행하는 객체: self
-charizard.attack(squirtle) # 주체(self)는 charizard임
-# print(pikachu.name) # error: Pokemen 객체는 name이라는 속성이 없음 - 1
-# print(squirtle.name)
+class Pikachu(Pokemon): # is-a
+    def __init__(self, name, type):
+        super().__init__(name) # super(): 부모 클래스의 정의를 얻음 즉 부모 클래스의 init method 정의
+        self.type = type
+    def attack(self, target): # 부모 클래스의 attack을 override(재정의)
+        print(f'{self.name}이(가) {target.name}을(를) {self.type} 공격!')
+    def electric_info(self):
+        print("전기 계열의 공격을 합니다.")
+
+class Squirtle(Pokemon): # is-a
+    pass
+
+class Agumon:
+    pass
+
+p1 = Pikachu("피카츄", "전기")
+p2 = Squirtle("꼬부기")
+p1.electric_info()
+# p2.electric_info() : error (electric_info()는 Pikachu에 정의된 method이기 때문
+p1.attack(p2)
+p2.attack(p1)
+print(p1.name, p1.type)
+print(p1.name)
+print(issubclass(Pikachu, Pokemon))
+print(issubclass(Agumon, Pokemon))

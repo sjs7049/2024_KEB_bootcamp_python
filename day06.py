8633a8785f8bdef5151a45d66663862e96352fce
sjs7049 <sjs7049@gmail.com>
2024-01-19 09:33:24 +0900

    v5.5 assignment 9.3(2) day06

--- day06.py
+++ day06.py
@@ -1,15 +1,17 @@
-
+# Open Closed Principle
 def test(f):
     '''
     decorator 함수, 함수 시작하면 start 출력, 함수 끝나면 end 출력
     :param f: function
     :return: closure function
     '''
-    def inner(*args, **kwargs): # *args라 인수가 없어도 됨
+    # def inner(*args, **kwargs)
+    def inner(): # *args라 인수가 없어도 됨
         print('start')
-        result = f(*args, **kwargs)
+        # result = f(*args, **kwargs)
+        f()
         print('end')
-        return result
+        # return result
     return inner
 
 @test

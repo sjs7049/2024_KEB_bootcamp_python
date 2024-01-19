c42fb9f033becbfe365d5e79dd34260bdaeddc8b
sjs7049 <sjs7049@gmail.com>
2024-01-19 13:02:15 +0900

    v5.9 python day06

--- day06.py
+++ day06.py
@@ -1,27 +1,6 @@
-import random
+class Pokemon:
+    def __init__(self, name):
+        print(f'{name} 포켓몬스터 생성')
 
-class OopsException(Exception):
-    pass
-
-# numbers = []
-# for i in range(5):
-#     numbers.append(random.randint(1,100))
-
-numbers = [random.randint(1,100) for i in range(10)]
-print(numbers)
-
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers) - 1}) : "))
-    print(numbers[pick])
-    raise OopsException("Oops~~")
-except IndexError as err: # as ~ 해주면 시스템이 던져주는 어떤 오류인지에 대한 문구도 출력
-    print(f'Wrong index number\n {err}')
-except ValueError as err:
-    print(f'Input Only Number!\n{err}')
-except OopsException as err:
-    print(f"Oops Oops {err}")
-except Exception as err: # detail한 예외 위에 적은 뒤 마지막에 넣어야 함
-    print(f"Error Occurs : {err}")
-else:
-    print(f'Program terminate')
-
+pikache = Pokemon('피키츄')
+squirtle = Pokemon('꼬부기')

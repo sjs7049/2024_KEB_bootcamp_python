b984db48436d0d7cf4ffd1c8f39a5be984f5c320
sjs7049 <sjs7049@gmail.com>
2024-01-19 16:46:04 +0900

    v6.3 python day06

--- day06.py
+++ day06.py
@@ -8,7 +8,19 @@ class SwimmingMixin:
 
 class Pokemon:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
+
+    def attack(self):
+        print("공격!!")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
 
 class Charizard(Pokemon, FlyingMixin):
     pass
@@ -18,5 +30,10 @@ class Gyarados(Pokemon, SwimmingMixin):
 
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-print(g1.swim())
-print(c1.fly())
+
+print(g1.name)
+# print(g1.__name): error - direct access 불가
+# g1.__name = "잉어킹" : 잉어킹으로 바꿔도 값이 바뀌지 않음
+g1._Pokemon__name = "잉어킹"
+print(g1._Pokemon__name) # 접근 방법
+

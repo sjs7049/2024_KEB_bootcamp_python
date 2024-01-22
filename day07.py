e908d51a6daa5fdda8ecb961b1b776be2ddd7616
sjs7049 <sjs7049@gmail.com>
2024-01-22 11:04:53 +0900

    v7.0 python day07

--- day07.py
+++ day07.py
@@ -1,14 +1,24 @@
-class FlyingMixin:
+class FlyingBehavior:
     def fly(self):
-        return f"{self.__name}이(가) 하늘을 훨훨 날아갑니다~"
+        return f'하늘을 훨훨 날아갑니다~'
 
-class SwimmingMixin:
+class Nofly(FlyingBehavior):
+    def fly(self):
+        return f'하늘을 날 수 업습니다.'
+
+class FlyWithWings(FlyingBehavior):
+    def fly(self):
+        return f'날개로 하늘을 훨훨 날아갑니다~'
+
+class SwimmingBehavior:
     def swim(self):
-        return f"{self.__name}이(가) 수영을 합니다."
+        return f'수영을 합니다.'
 
 class Pokemon:
-    def __init__(self, name):
+    def __init__(self, name, hp, fly):
         self.__name = name
+        self.hp = hp
+        self.fly_behavior = fly # aggregation (has-a)
 
     def attack(self):
         print("공격~")
@@ -22,21 +32,27 @@ class Pokemon:
         self.__name = new_name
 
     #name = property(get_name, set_name)
-    # magic method
+    # magic method 1
     def __str__(self):
         return self.__name + "입니다"
 
+    # magic method 2
     def __add__(self, target):
-        return self.__name + " + " + target.__name
+        # return self.__name + " + " + target.__name
+        return f'두 포켓몬 체력의 합은 {self.hp + target.hp}입니다.'
 
-class Charizard(Pokemon, FlyingMixin):
+class Charizard(Pokemon):
     pass
 
-class Gyarados(Pokemon, SwimmingMixin):
+class Pikachu(Pokemon):
     pass
 
-g1 = Gyarados("갸라도스")
-c1 = Charizard("리자몽")
-print(g1)
+nofly = Nofly()
+p1 = Pikachu("피카츄", 35, nofly)
+wings = FlyWithWings()
+c1 = Charizard("리자몽", 120, wings)
+print(c1.fly_behavior.fly())
+print(p1.fly_behavior.fly())
+print(p1)
 print(c1)
-print(g1+c1)
+print(p1+c1)
